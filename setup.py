4d118443165a4c599fcad6c36e03e7f2bc1783ef
Bjorkor <23528137+Bjorkor@users.noreply.github.com>
2024-05-29 11:42:31 -0500

    .02

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 # setup.py
 from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='excel_normalizer',
-    version='0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
